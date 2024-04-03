83bd15ea30db1c4986608cb7d9733838ddc6b75d
Ogordiunor Uche Jude <excelxz78@gmail.com>
2024-04-03 11:21:55 -0700

    new additions

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["3.94.213.135", "100.27.2.37"]
 
 
 def do_pack():
